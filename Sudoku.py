b7ae876c267c6b60b2a82896e9aae30c8622de6f
Tim-Smet <tim2smet@gmail.com>
2020-03-22 17:19:36 +0100

    Finished but there is still something missing

--- Sudoku.py
+++ Sudoku.py
@@ -10,6 +10,44 @@ sudoku = [
     [0,4,7,9,0,5,0,8,6]
 ]
 
+def solve(board):
+    find = find_zero(board)
+    if not find:
+        return True
+    else:
+        row, col = find
+
+    for i in range(1,10):
+        if check_number_valid(board, i, (row, col)):
+            board[row][col] = i
+
+            if solve(board):
+                return True
+            
+            board[row][col] = 0
+    
+    return False
+
+def check_number_valid(board, number, position):
+    #Check row
+    for i in range(9):
+        if board[position[0]][i] == number and position[1] != i:
+            return False
+
+    #Check column
+    for i in range(9):
+        if board[i][position[1]] == number and position[0] != i:
+            return False      
+
+    #Check box
+    box_x = position[1] // 3
+    box_y = position[0] // 3
+
+    for i in range(box_y * 3, box_y * 3 + 3):
+        for j in range(box_x * 3, box_x * 3 + 3):
+            if board[i][j] == number and (i, j) != position:
+                return False
+
 def print_board(board):
 
     for i in range(9):
@@ -25,11 +63,16 @@ def print_board(board):
             else:
                 print(str(board[i][j]) + " ", end="")
 
-print_board(sudoku)
-
 def find_zero(board):
     for i in range(9):
         for j in range(9):
             if board[i][j] == 0:
                 return (i, j)
 
+    return None
+
+
+print_board(sudoku)
+solve(sudoku)
+print("-------------------")
+print_board(sudoku)
